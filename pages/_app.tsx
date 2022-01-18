0a5458731a4992446211f14136c1e6a3e6d74b73
thejahid <i.thejahid@gmail.com>
2022-01-18 16:31:53 +0600

    dev start

---
+++ pages/_app.tsx
@@ -0,0 +1,10 @@
+import type { AppProps } from "next/app";
+
+// Tailwind css
+import "styles/tailwind.css";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
